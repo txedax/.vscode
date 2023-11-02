aa226eeedb0352ae144096cb5b7e19ed2ceb2515
Almaz <yaeval.wk@gmail.com>
2023-11-03 04:44:41 +0600

    initial commit

---
+++ extensions.sh
@@ -0,0 +1,18 @@
+# This list is not up to date.
+code --install-extension EditorConfig.EditorConfig
+code --install-extension usernamehw.errorlens
+code --install-extension dbaeumer.vscode-eslint
+code --install-extension miguelsolorio.fluent-icons
+code --install-extension eamodio.gitlens
+code --install-extension VisualStudioExptTeam.intellicode-api-usage-examples
+code --install-extension Zignd.html-css-class-completion
+code --install-extension ms-vscode.live-server
+code --install-extension ms-vsliveshare.vsliveshare
+code --install-extension PKief.material-icon-theme
+code --install-extension IBM.output-colorizer
+code --install-extension pnp.polacode
+code --install-extension esbenp.prettier-vscode
+code --install-extension stylelint.vscode-stylelint
+code --install-extension earshinov.sort-lines-by-selection
+code --install-extension mattpocock.ts-error-translator
+code --install-extension ChakrounAnas.turbo-console-log
